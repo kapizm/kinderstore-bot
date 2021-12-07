71581d9d752cfd83946dffebf53141a22675b68d
kapizm <m.kapiz@astanait.edu.kz>
2021-12-08 01:38:31 +0600

    create dialog

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,49 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 from src import config
+from telegram import ReplyKeybordMarkup, ReplyKeybordRemove, Update
 
 
 bot = Updater(token=config.TOKEN)
 
 
-def start_handler(update, context):
-    update.message.reply_text("Test")
+logging.basicConfig(
+    format=format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+def start_handler(update: UPDATE, context: CallbackContext) -> int:
+    reply_keyboard = [['Зарегистрировать чек', 'Проверить свои чеки']]
+
+    update.message.reply_text(
+        'Здравствуйте! Вы бы хотели зарегистрировать в нашем розыгрыше, или проверить ваши зарегистрированные чеки?'
+        'Отправьте /cancel чтобы остановить работу бота\n\n',
+        reply_markup=ReplyKeybordMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Регистрация или проверка?'
+        ),
+    )
+
+    return CHOICE
+
+def registration(update, context) -> int:
+    update.message.reply_text("""Здравствуйте! Рады приветствовать Вас в розыгрыше «Игрушки покупай – машину забирай» от сети детских магазинов KinderStore! 
+Розыгрыш состоится в прямом эфире на нашей инстаграм странице Kinderstore_astana, подпишитесь что быть в курсе!
+С условиями конкурса вы можете ознакомиться по данной ссылке ______
+Дата розыгрыша 10 января 2022 года в г. Нур-Султан, в 14:00 по местному времени. 
+Тех. поддержка +7 702 8 777 """)
+    update.message.reply_text("""Для успешной регистрации в розыгрыше Вам необходимо ввести своё имя и фамилию: 
+    """)
+
+    return NAME
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'До свидания!', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END    
 
 
 bot.dispatcher.add_handler(CommandHandler("start", start_handler))
