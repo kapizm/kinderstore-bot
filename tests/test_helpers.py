78420a850c21a2abdc8d67801b8dde487f896310
kapizm <m.kapiz@astanait.edu.kz>
2021-12-12 16:31:37 +0600

    delete function get total chances

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,8 +1,6 @@
-from typing import List
-
 import pytest
 
-from src.helpers import get_chances_from_price, get_total_chances
+from src.helpers import get_chances_from_price
 
 
 @pytest.mark.parametrize(
@@ -17,17 +15,3 @@ from src.helpers import get_chances_from_price, get_total_chances
 )
 def test_get_chances_from_price(price: int, chances: int):
     assert get_chances_from_price(price) == chances
-
-
-@pytest.mark.parametrize(
-    'chances_list, total_chances',
-    [
-        ([], 0),
-        ([1], 1),
-        ([1, 0], 1),
-        ([0, 0], 0),
-        ([1, 1], 2),
-    ],
-)
-def test_get_total_chances(chances_list: List[int], total_chances: int):
-    assert get_total_chances(chances_list) == total_chances
