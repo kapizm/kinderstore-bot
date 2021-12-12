4b01158a3f54a5af08e26274313286007056a24f
qwanysh <olimpsdu@gmail.com>
2021-12-12 16:16:03 +0600

    add get_total_chances tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,8 @@
+from typing import List
+
 import pytest
 
-from src.helpers import get_chances_from_price
+from src.helpers import get_chances_from_price, get_total_chances
 
 
 @pytest.mark.parametrize(
@@ -15,3 +17,17 @@ from src.helpers import get_chances_from_price
 )
 def test_get_chances_from_price(price: int, chances: int):
     assert get_chances_from_price(price) == chances
+
+
+@pytest.mark.parametrize(
+    'chances_list, total_chances',
+    [
+        ([], 0),
+        ([1], 1),
+        ([1, 0], 1),
+        ([0, 0], 0),
+        ([1, 1], 2),
+    ],
+)
+def test_get_total_chances(chances_list: List[int], total_chances: int):
+    assert get_total_chances(chances_list) == total_chances
