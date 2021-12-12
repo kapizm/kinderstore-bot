76da1fc37c8628a4f48661ae449988490ac94565
kapizm <m.kapiz@astanait.edu.kz>
2021-12-12 16:09:54 +0600

    add get chances from price

--- src/helpers.py
+++ src/helpers.py
@@ -1,2 +1,12 @@
-def get_chances_from_price(price: int):
-    return 0
+def get_chances_from_price(price: int) -> int:
+    if price < 25_000:
+        return 0
+
+    price -= 25_000
+    chances = 1
+
+    while price >= 20_000:
+        chances += 1
+        price -= 20_000
+
+    return chances
