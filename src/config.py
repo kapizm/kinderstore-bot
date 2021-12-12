9f825b0455d412771f0a43c544966cecd66defe2
qwanysh <olimpsdu@gmail.com>
2021-12-12 22:53:09 +0600

    add get_check_data_from_api

--- src/config.py
+++ src/config.py
@@ -11,3 +11,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 PORT = os.getenv('PORT')
 
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+API_BASE_URL = os.getenv('API_BASE_URL')
+
+API_TOKEN = os.getenv('API_TOKEN')
