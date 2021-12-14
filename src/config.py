42daa3abf17fe679fce6bab889eea0b964957e8c
qwanysh <olimpsdu@gmail.com>
2021-12-14 20:17:15 +0600

    fix database url driver

--- src/config.py
+++ src/config.py
@@ -10,7 +10,10 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 PORT = os.getenv('PORT')
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL', '')
+
+if DATABASE_URL.startswith('postgres://'):
+    DATABASE_URL = DATABASE_URL.replace('postgres', 'postgresql')
 
 API_BASE_URL = os.getenv('API_BASE_URL')
 
