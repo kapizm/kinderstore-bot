0b57bbe4161cd171c29f3137cf9a7d557b7dafbc
kapizm <m.kapiz@astanait.edu.kz>
2021-12-14 14:41:35 +0600

    can't launch code

--- src/handlers/my_checks.py
+++ src/handlers/my_checks.py
@@ -11,14 +11,13 @@ def my_checks_handler(update: Update, context: CallbackContext):
     with database.Session() as session:
         user = session.query(User).filter(
             User.telegram_id == update.callback_query.from_user.id,
-        ).join(User.checks).first()
+        ).join(User.checks).encode('utf-8').first()
 
         if user.checks:
             for check in user.checks:
-                text += f'• {check.number}\n'
-
+                text += f'• {check.number}\n '
         else:
-            update.message.reply('У вас пока нет зарегистрированных чеков')
+            text = 'У вас пока нет зарегистрированных чеков'
 
     buttons = [
         [
